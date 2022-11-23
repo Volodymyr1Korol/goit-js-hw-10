3547949ee1921522516e7377545c1023fbb62252
Volodymyr1Korol <volodymyrkorol18@gmail.com>
2022-11-23 09:31:23 +0200

    Update fetchCountries

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,19 +1,19 @@
- BASE_URL = 'https://restcountries.com/v3.1/name';
+const BASE_URL = 'https://restcountries.com/v3.1/name';
 
- export const fetchCountries = name => {
-     return fetch(
-             `https://restcountries.com/v3.1/name/${name}?fields=,name,capital,population,flags,languages`
-         )
-         .then(response => {
-             if (!response.ok) {
-                 if (response.status === 404) {
-                     return [];
-                 }
-                 throw new Error(response.status);
-             }
-             return response.json();
-         })
-         .catch(error => {
-             console.error(error);
-         });
- };
+export const fetchCountries = name => {
+    return fetch(
+            `https://restcountries.com/v3.1/name/${name}?fields=,name,capital,population,flags,languages`
+        )
+        .then(response => {
+            if (!response.ok) {
+                if (response.status === 404) {
+                    return [];
+                }
+                throw new Error(response.status);
+            }
+            return response.json();
+        })
+        .catch(error => {
+            console.error(error);
+        });
+};
